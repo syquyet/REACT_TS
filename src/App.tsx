70ebf7942b64a63f9cf11b1c80ac1d36e5716852
syquyet <quyetvosy@gmail.com>
2023-09-28 09:41:53 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,88 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+  <header>
+    <h1>session 02 -tabe- module 01- ngay 01/08/2023</h1>
+  </header>
+  <main>
+    <section>
+      <h3>vi du the table</h3>
+      <table border={1}>
+        <tbody>
+          <tr>
+            <th>cot 1</th>
+            <th>cot 2</th>
+            <th>cot 3</th>
+          </tr>
+          <tr>
+            <td>cot 1</td>
+            <td>cot 2</td>
+            <td>cot 3</td>
+          </tr>
+        </tbody>
+      </table>
+      <table border={1}>
+        <tbody>
+          <tr>
+            <th>1.1</th>
+            <th colSpan={2}>1.2</th>
+            <th>1.3</th>
+          </tr>
+          <tr>
+            <td rowSpan={3}>2.1 3.1 4.1</td>
+            <td>2.2</td>
+            <td>2.3</td>
+          </tr>
+          <tr>
+            <td>3.1</td>
+            <td>3.2</td>
+            <td>3.3</td>
+          </tr>
+          <tr>
+            <td>4.1</td>
+            <td>4.2</td>
+            <td>4.3</td>
+          </tr>
+        </tbody>
+        <caption>bang 1</caption>
+      </table>
+    </section>
+    <section>
+      <h3>cac the cua table</h3>
+      <p>the capsion mo ta bang</p>
+      <p>the thead phaan chia phaan ddau</p>
+      <p>the tbody</p>
+      <p>the tfoot</p>
+    </section>
+    <section>
+      <h3>vi du cac the tabe thead tbody tfoot</h3>
+      <table>
+        <tbody />
+        <thead>
+          <tr>
+            <th>mon</th>
+            <th>tue</th>
+            <th>wue</th>
+            <th>thu</th>
+            <th>sat</th>
+            <th>sun</th>
+          </tr>
+        </thead>
+      </table>
+    </section>
+    <section>
+      <h3>mot so the moi HTML5</h3>
+      <p>thien trinh lam viec</p>
+      <div>
+        <progress max={100} value={80} /> <span>80%</span>
+      </div>
+      <progress max={100} value={80} /> <span>80%</span>
+    </section>
+    
+  </main>
+</>
+
     </div>
   );
 }
