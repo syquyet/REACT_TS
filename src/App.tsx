ac388bfb3ddd69064e42fe417d3d11500a6df40c
syquyet <quyetvosy@gmail.com>
2023-10-02 10:27:57 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super({});
+  }
+  state: { count: number } = { count: 1 };
+  render() {
+    return (
+      <div className="App">
+        <h1>xin chào session_11</h1>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          click
+        </button>
+        <h2>{this.state.count}</h2>
+        {true && <Child />}
+      </div>
+    );
+  }
 }
 
 export default App;
+class Child extends Component {
+  render(): React.ReactNode {
+    // các trường hợp chạy render khi mouting chạy say  componentWillMount
+    // 2 chạy khi update chạy sau componentWillUpdate --> update props, update state
+    console.log("render.....");
+    return (
+      <div>
+        <h1>ví dụ</h1>
+      </div>
+    );
+  }
+  componentWillMount(): void {
+    console.log("componentWillMount.....");
+    // chạy khi đc gắn vào chạy lần đầu
+  }
+  componentDidMount(): void {
+    console.log("componentDidMount.....");
+    // chạy một lần sau khi render
+  }
+  componentWillUnmount(): void {
+    console.log("componentWillUnmount...");
+  }
+  shouldComponentUpdate(): boolean {
+    return true;
+  }
+  // chạy khi thay đổi props hay state
+  componentWillReceiveProps(): void {
+    console.log("componentWillReceiveProps......");
+  }
+// chạy khi thay đổi props hay state
+  componentWillUpdate(): void {
+    console.log("componentWillUpdate.....");
+  }
+// chạy khi thay đổi props hay state
+  componentDidUpdate(): void {
+    console.log("componentDidUpdate.....");
+  }
+}
